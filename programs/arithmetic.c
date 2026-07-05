a91a3744458d12ae9f1d79dd5c68446cc205da8c
Abraham <abt150@gmail.com>
2026-07-05 16:45:08 -0500

    minor comment tweaks

--- programs/arithmetic.c
+++ programs/arithmetic.c
@@ -4,31 +4,18 @@
 
 int main(void) {
     /* Program to demonstrate arithmetic operations */
-    /* Stack progression:
-       push 5           -> [5]
-       push 3           -> [5, 3]
-       add              -> [8]
-       push 10          -> [8, 10]
-       push 2           -> [8, 10, 2]
-       mul              -> [8, 20]
-       sub              -> [-12]
-       push 4           -> [-12, 4]
-       div              -> [-3]
-       print            -> prints -3
-       halt             -> stop
-    */
     uint8_t program[] = {
-        OP_PUSH, 5,
-        OP_PUSH, 3,
-        OP_ADD,
-        OP_PUSH, 10,
-        OP_PUSH, 2,
-        OP_MUL,
-        OP_SUB,
-        OP_PUSH, 4,
-        OP_DIV,
-        OP_PRINT,
-        OP_HALT
+        OP_PUSH, 5,  /* -> [5] */
+        OP_PUSH, 3,  /* -> [5, 3] */
+        OP_ADD,      /* -> [8] */
+        OP_PUSH, 10, /* -> [8, 10] */
+        OP_PUSH, 2,  /* -> [8, 10, 2] */
+        OP_MUL,      /* -> [8, 20] */
+        OP_SUB,      /* -> [-12] */
+        OP_PUSH, 4,  /* -> [-12, 4] */
+        OP_DIV,      /* -> [-3] */
+        OP_PRINT,    /* prints -3 */
+        OP_HALT      /* stop */
     };
 
     VM vm;
--- programs/stack_ops.c
+++ programs/stack_ops.c
@@ -4,31 +4,18 @@
 
 int main(void) {
     /* Program to demonstrate the new stack manipulation opcodes */
-    /* Stack progression:
-       push 1           -> [1]
-       push 2           -> [1, 2]
-       push 3           -> [1, 2, 3]
-       swap             -> [1, 3, 2]
-       print            -> pops 2, leaving [1, 3]
-       drop             -> pops 3, leaving [1]
-       push 4           -> [1, 4]
-       swap             -> [4, 1]
-       print            -> pops 1, leaving [4]
-       print            -> pops 4, leaving []
-       halt             -> stop
-    */
     uint8_t program[] = {
-        OP_PUSH, 1,
-        OP_PUSH, 2,
-        OP_PUSH, 3,
-        OP_SWAP,
-        OP_PRINT,
-        OP_DROP,
-        OP_PUSH, 4,
-        OP_SWAP,
-        OP_PRINT,
-        OP_PRINT,
-        OP_HALT
+        OP_PUSH, 1, /* -> [1] */
+        OP_PUSH, 2, /* -> [1, 2] */
+        OP_PUSH, 3, /* -> [1, 2, 3] */
+        OP_SWAP,    /* -> [1, 3, 2] */
+        OP_PRINT,   /* pops 2, leaving [1, 3] */
+        OP_DROP,    /* pops 3, leaving [1] */
+        OP_PUSH, 4, /* -> [1, 4] */
+        OP_SWAP,    /* -> [4, 1] */
+        OP_PRINT,   /* pops 1, leaving [4] */
+        OP_PRINT,   /* pops 4, leaving [] */
+        OP_HALT     /* stop */
     };
 
     VM vm;
