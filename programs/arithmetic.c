fcee9b8d0f412173ee659330cb7b6b064677b8ee
Abraham <abt150@gmail.com>
2026-07-05 16:10:47 -0500

    touch up programs

--- programs/arithmetic.c
+++ programs/arithmetic.c
@@ -4,6 +4,19 @@
 
 int main(void) {
     /* Program to demonstrate arithmetic operations */
+    /* Stack progression:
+       push 5           -> [5]
+       push 3           -> [5, 3]
+       add              -> [8]
+       push 10          -> [8, 10]
+       push 2           -> [8, 10, 2]
+       mul              -> [8, 20]
+       sub              -> [-12]
+       push 4           -> [-12, 4]
+       div              -> [-3]
+       print            -> prints -3
+       halt             -> stop
+    */
     uint8_t program[] = {
         OP_PUSH, 5,
         OP_PUSH, 3,
--- programs/stack_ops.c
+++ programs/stack_ops.c
@@ -4,14 +4,29 @@
 
 int main(void) {
     /* Program to demonstrate the new stack manipulation opcodes */
+    /* Stack progression:
+       push 1           -> [1]
+       push 2           -> [1, 2]
+       push 3           -> [1, 2, 3]
+       swap             -> [1, 3, 2]
+       print            -> pops 2, leaving [1, 3]
+       drop             -> pops 3, leaving [1]
+       push 4           -> [1, 4]
+       swap             -> [4, 1]
+       print            -> pops 1, leaving [4]
+       print            -> pops 4, leaving []
+       halt             -> stop
+    */
     uint8_t program[] = {
         OP_PUSH, 1,
         OP_PUSH, 2,
-        OP_DUP,
+        OP_PUSH, 3,
         OP_SWAP,
-        OP_OVER,
         OP_PRINT,
         OP_DROP,
+        OP_PUSH, 4,
+        OP_SWAP,
+        OP_PRINT,
         OP_PRINT,
         OP_HALT
     };
