59657d3c223231f70f6c5111eb6dfd159bf1f5f6
Abraham <abt150@gmail.com>
2026-07-05 15:30:02 -0500

    fancier program

--- main.c
+++ main.c
@@ -3,13 +3,24 @@
 #include "opcodes.h"
 #include "vm.h"
 
-/* Assembled code */
 int main(void) {
-    /* Example program: push 5, push 3, add, print, halt */
+    // Program to demonstrate the virtual machine operations
+    // ADD: 5 + 3 = 8
+    // MUL: 10 * 2 = 20
+    // SUB: 8 - 20 = -12
+    // DIV: -12 / 4 = -3
+    // PRINT: -3
+    
     uint8_t program[] = {
         OP_PUSH, 5,
         OP_PUSH, 3,
         OP_ADD,
+        OP_PUSH, 10,
+        OP_PUSH, 2,
+        OP_MUL,
+        OP_SUB,
+        OP_PUSH, 4,
+        OP_DIV,
         OP_PRINT,
         OP_HALT
     };
