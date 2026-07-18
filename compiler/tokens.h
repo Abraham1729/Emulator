dfc50beecad4772470702e17de5db430eb19b6be
Abraham1729 <77895428+Abraham1729@users.noreply.github.com>
2026-07-18 16:27:59 -0500

    fix token printer to handle EOF

--- compiler/tokens.h
+++ compiler/tokens.h
@@ -44,7 +44,7 @@ static inline const char *token_type_name(TokenType type) {
         [TOK_EOF]     = "EOF",
     };
 
-    if (type < 0 || type > TOK_COMMENT) {
+    if (type < 0 || type > TOK_EOF) {
         return "UNKNOWN";
     }
     return names[type];
